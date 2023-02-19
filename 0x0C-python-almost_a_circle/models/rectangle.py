efd2d0bee400aacb9407a156ef993772024c1344
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2023-02-19 03:58:41 +0100

    trying to fix import error

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,4 +1,7 @@
-from base import Base
+try:
+    from models.base import Base
+except ModuleNotFoundError:
+    Base = __import__('base').Base
 
 
 class Rectangle(Base):
@@ -76,14 +79,53 @@ class Rectangle(Base):
         """Displays the rectangle using # """
         for y in range(self.y):
             print("")
-        for row in range(self.__height):
+        for column in range(self.__height):
             for x in range(self.x):
                 print(" ", end="")
-            for column in range(self.__width):
+            for row in range(self.__width):
                 print("#", end="")
             print()
 
+    def __str__(self):
+        """Defines a format for the string representation of the class"""
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - / {self.__width}/{self.__height}"
 
+    def update(self, *args):
+        """Assigns an argument to each attribute"""
+
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
+
+        # elif kwargs and len(kwargs) != 0:
+        #     for k, v in kwargs.items():
+        #         if k == "id":
+        #             if v is None:
+        #                 self.__init__(self.width, self.height, self.x, self.y)
+        #             else:
+        #                 self.id = v
+        #         elif k == "width":
+        #             self.width = v
+        #         elif k == "height":
+        #             self.height = v
+        #         elif k == "x":
+        #             self.x = v
+        #         elif k == "y":
+        #             self.y = v
 
 # b1 = Base()
 # print(b1.id)
@@ -101,61 +143,84 @@ class Rectangle(Base):
 # print(b5.id)
 # print("-----------")
 
-r1 = Rectangle(10, 2)
-print(r1.id)
+# r1 = Rectangle(10, 2)
+# print(r1.id)
 
-r2 = Rectangle(2, 10)
-print(r2.id)
+# r2 = Rectangle(2, 10)
+# print(r2.id)
 
-r3 = Rectangle(10, 2, 0, 0, 12)
-print(r3.id)
-print("-------------")
+# r3 = Rectangle(10, 2, 0, 0, 12)
+# print(r3.id)
+# print("-------------")
 
-try:
-    Rectangle(10, "2")
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+# try:
+#     Rectangle(10, "2")
+# except Exception as e:
+#     print("[{}] {}".format(e.__class__.__name__, e))
 
-try:
-    r = Rectangle(10, 2)
-    r.width = -10
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+# try:
+#     r = Rectangle(10, 2)
+#     r.width = -10
+# except Exception as e:
+#     print("[{}] {}".format(e.__class__.__name__, e))
 
-try:
-    r = Rectangle(10, 2)
-    r.x = {}
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+# try:
+#     r = Rectangle(10, 2)
+#     r.x = {}
+# except Exception as e:
+#     print("[{}] {}".format(e.__class__.__name__, e))
 
-try:
-    Rectangle(10, 2, 3, -1)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-print("-----------------------------")
+# try:
+#     Rectangle(10, 2, 3, -1)
+# except Exception as e:
+#     print("[{}] {}".format(e.__class__.__name__, e))
+# print("-----------------------------")
 
-r1 = Rectangle(3, 2)
-print(r1.area())
+# r1 = Rectangle(3, 2)
+# print(r1.area())
 
-r2 = Rectangle(2, 10)
-print(r2.area())
+# r2 = Rectangle(2, 10)
+# print(r2.area())
 
-r3 = Rectangle(8, 7, 0, 0, 12)
-print(r3.area())
-print("----------------")
+# r3 = Rectangle(8, 7, 0, 0, 12)
+# print(r3.area())
+# print("----------------")
 
-r1 = Rectangle(4, 6)
-r1.display()
+# r1 = Rectangle(4, 6)
+# r1.display()
+# print("---")
 
-print("---")
+# r1 = Rectangle(4, 6, 2, 1, 12)
+# print(r1)
 
-r1 = Rectangle(2, 2)
-r1.display()
+# r2 = Rectangle(5, 5, 1)
+# print(r2)
+# print("------------------")
 
-r1 = Rectangle(2, 3, 2, 2)
-r1.display()
+# r1 = Rectangle(2, 3, 2, 2)
+# r1.display()
 
-print("---")
+# print("---")
 
-r2 = Rectangle(3, 2, 1, 0)
-r2.display()
+# r2 = Rectangle(3, 2, 1, 0)
+# r2.display()
+# print("--------------------")
+
+
+# r1 = Rectangle(10, 10, 10, 10)
+# print(r1)
+
+# r1.update(89)
+# print(r1)
+
+# r1.update(89, 2)
+# print(r1)
+
+# r1.update(89, 2, 3)
+# print(r1)
+
+# r1.update(89, 2, 3, 4)
+# print(r1)
+
+# r1.update(89, 2, 3, 4, 5)
+# print(r1)
