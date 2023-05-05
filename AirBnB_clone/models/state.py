6cb6542009d2e916207b857291e8db5495079e7a
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2023-05-05 15:47:52 +0100

    push

---
+++ AirBnB_clone/models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""creates a class called state """
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """class State which inherits from BaseModel"""
+    name = ""
