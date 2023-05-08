64d84fd9783ffa367cadb847d472ff47dcdd969e
meetkamal256 <meetkamal256@gmail.com>
2023-05-08 23:45:21 +0100

    fixed connection error

--- AirBnB_V2/setup_mysql_dev.sql
+++ AirBnB_V2/setup_mysql_dev.sql
@@ -1,7 +1,6 @@
 -- script that prepares a MySQL server for the project
 -- creates database and user
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' 
-       IDENTIFIED BY 'Kamal256$';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'  IDENTIFIED BY 'Kamal256$';
  GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
  GRANT SELECT ON performance_schema.* to 'hbnb_dev'@'localhost';
