8b74e4e918b4afe47b1915ca27fb52e7419016da
meetkamal256 <meetkamal256@gmail.com>
2023-05-09 03:03:14 +0100

    it is now working

--- AirBnB_V2/env.sh
+++ AirBnB_V2/env.sh
@@ -3,4 +3,4 @@ export HBNB_MYSQL_USER=hbnb_dev \
 export HBNB_MYSQL_PWD="Kamal256$" \
 export HBNB_MYSQL_HOST=localhost \
 export HBNB_MYSQL_DB=hbnb_dev_db \
-export HBNB_TYPE_STORAGE=db && ./console.py
+export HBNB_TYPE_STORAGE=db
